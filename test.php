5e30a02abeebeb281fea6bea11cc4da80e2aa253
ikkehmochi <abdurrahmanalhakim03@gmail.com>
2023-06-04 20:34:50 +0700

    deletedirrelevant files

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once("conn.php");
-date_default_timezone_set("Asia/Bangkok");
-$query="SELECT id_booking, nama_dipesan FROM booking LEFT JOIN meja ON booking.no_meja_fk = meja.no_meja WHERE booking.no_meja_fk=2;";
-$reserve_details=$GLOBALS['conn']->query($query)->fetch();
-return $reserve_details;
-?>
